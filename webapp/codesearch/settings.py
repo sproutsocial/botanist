3904f3dd15491a28fb1201c82b85d7c19ed1634d
Michael Sphar <91095350+mikespharss@users.noreply.github.com>
2024-06-17 16:00:48 -0500

    TOOLCM-2143 - Add observability integrations (#43)
    Add datadog and sentry integrations to codesearch django webapp
    and the github_backup.py script
    
    webapp:
      * Add datadog and sentry-sdk to pyproject.toml
      * Remove `--plugin python311` from uwsgi run command. It logs a 'not found' error and it not needed. The plugin is build into the uwsgi binary when installed via pip.
      * Add --enable-threads and --py-call-uwsgi-fork-hooks options to uwsgi run command per Sentry docs for django integration
      * Add sentry package and initialization to codesearch webapp settings
      * Lots of updates to poetry.lock
    
    github_backup.py
      * Add datadog and sentry
      * emit a counter metric for backed up repos
      * script should use the same python environment the webapp uses
      * Add logging of repositories skipped due to being archived

--- webapp/codesearch/settings.py
+++ webapp/codesearch/settings.py
@@ -8,6 +8,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
+import sentry_sdk
+sentry_sdk.init()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
