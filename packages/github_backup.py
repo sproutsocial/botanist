2a1c456cae956b36a830aa7d5662c5e07ba290ba
Joe Trollo <joetrollo@users.noreply.github.com>
2024-07-08 10:48:33 -0700

    Catch missing link header
    In Python 2, `response.headers["header-key"]` would return a `KeyError` if the header wasn't present, but in Python 3 it returns `None`. This is currently the top source of errors in Sentry.

--- packages/github_backup.py
+++ packages/github_backup.py
@@ -102,11 +102,11 @@ def get_repos(org, repo_type, access_token=None, username=None, password=None, p
         raise ValueError('unworkable combination of authentication inputs')
 
     response = urlopen(request)
-    try:
-        pagination = get_pagination(response.headers['Link'])
-    except KeyError:
+    raw_link_header = response.headers.get('Link')
+    if raw_link_header is None:
         logging.debug('no Link header, nothing to paginate through.')
         pagination = Pagination(None, None, None, None)
+    pagination = get_pagination(raw_link_header)
 
     repos = json.loads(response.read())
     for r in repos:
